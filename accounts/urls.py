1a0b1f7edacf889181844128161d69f4f181be10
Daucko <dauda.a.tijani@gmail.com>
2025-03-05 15:06:37 +0000

    Remove ProfileView URL path from accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from .home  import HomepageView
 from .register import RegisterView
-from .login import LoginView, Verify2FAView, Enable2FAView, ProfileView
+from .login import LoginView, Verify2FAView, Enable2FAView
 from django.urls import path
 from .email_verification import EmailVerificationView
 
@@ -12,7 +12,7 @@ urlpatterns = [
        path('register', RegisterView.as_view(), name='register'),
        path('login', LoginView.as_view(), name='login'),
        path('verify-email', EmailVerificationView.as_view(), name='verify'),
-       path('profile/', ProfileView.as_view(), name='profile'),
+       # path('profile/', ProfileView.as_view(), name='profile'),
        path('enable-2fa/', Enable2FAView.as_view(), name='enable_2fa'),
        path('verify-2fa/', Verify2FAView.as_view(), name='verify_2fa'),
        
